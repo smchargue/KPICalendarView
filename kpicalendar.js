8b1b66f3e6dc20b0fa492a490920271ac493bcc4
Steve McHargue <smchargue@outlook.com>
2022-06-28 13:18:10 -0500

    June 28 Buiild

--- kpicalendar.js
+++ kpicalendar.js
@@ -9,31 +9,44 @@ class kpiCalendar {
 
         // activeDate is the first day of the month of the current calendar view.
         this.activeDate = new Date(this.#validYear(year), this.#validMonth(month), 1);
-        this.elem = jQuery(selector || '.kpic-calendar, .kpic-calendar-sm').first();
-        
+        this.elem = jQuery(selector || '.kpic-calendar, .kpic-calendar-fill').first();
+
+        this.type = (this.elem.hasClass('kpic-calendar')) ? 'standard' : 'fill';
+
         // if true then do not display week 7 if not in current month, false (default) show only last week of view 
         this.truncateView = typeof this.elem.data("truncateview") === 'undefined' ? 1 : this.elem.data("truncateview");
         if (this.elem.data('monthdisplay')) {
-            var fmt = this.elem.data('monthformat') || 'long'; 
-            this.setMonthDisplay(this.elem.data('monthdisplay'),fmt);
+            var fmt = this.elem.data('monthformat') || 'long';
+            this.setMonthDisplay(this.elem.data('monthdisplay'), fmt);
         }
 
-        this.legendItems = [];
+        this.legendSelector = this.elem.data('legend');
+
+        this.kpiItems = [];
         this.#buildCalendarElement();
-        
+
     }
 
     // Public Methods 
+    setLegendSelector(s) {
+        s = s  || '';
+        if (s === '' && this.legendSelector) {
+            // if s is blank, and there was a previous legendSelector, hide it. 
+            jQuery(this.legendSelector).hide();
+        } else {
+            this.#buildLegend();
+        }
+    }
     setMonthDisplay(selector, format, callback) {
         this.monthDisplay = {
-            elem : jQuery(selector),
-            format : format || 'long',
-            callback : callback || null
+            elem: jQuery(selector),
+            format: format || 'long',
+            callback: callback || null
         }
-        this.#updateMonthName();        
+        this.#updateMonthName();
     }
 
-    setMonth(month, year, callback) {        
+    setMonth(month, year, callback) {
         var newDate = new Date(this.#validYear(year), this.#validMonth(month), 1);
         if (newDate.toDateString() !== this.activeDate.toDateString()) {
             this.activeDate = new Date(newDate);
@@ -44,7 +57,7 @@ class kpiCalendar {
         };
     }
 
-    addMonth(v,callback) {
+    addMonth(v, callback) {
         if (!parseInt(v)) {
             return;
         }
@@ -53,27 +66,76 @@ class kpiCalendar {
         this.setMonth(nd.getMonth(), nd.getFullYear(), callback);
     }
 
-    addItem(key, cssClass, title, text, color, uid) {
-        // add a legend item, 
-        var legendItem = {
-            key: key,
-            cssClass: cssClass || 'kpic-data-item-default',
-            title: title || '',
-            text : text || '',
-            color: color || '',
-            uid : uid || this.#uuid()
+    addItem(key, label, cssClass, title, text, color, uid) {
+
+        if (typeof key === 'object') {
+            var kpiItem = {
+                key: key.key,
+                label: key.label || '',
+                cssClass: key.cssClass || 'kpic-data-item-default',
+                title: key.title || '',
+                text: key.text || '',
+                color: key.color || '',
+                uid: key.uid || this.#uuid()
+            }            
+        } else {
+            var kpiItem = {
+                key: key,
+                label: label || '',
+                cssClass: cssClass || 'kpic-data-item-default',
+                title: title || '',
+                text: text || '',
+                color: color || '',
+                uid: uid || this.#uuid()
+            }
         }
-        this.legendItems.push(legendItem);
-        this.#addKpiDomItem(legendItem);
+        this.kpiItems.push(kpiItem);
+        this.#addKpiDomItem(kpiItem);
+
     }
 
     resetItems() {
-        this.legendItems = [];
+        this.kpiItems = [];
         this.elem.find('.kpic-data-item').remove();
     }
 
     getISODate() {
-        return this.activeDate.toISOString().slice(0,10);
+        return this.activeDate.toISOString().slice(0, 10);
+    }
+
+    #buildLegend(selector) {
+        function getLegend(item) {
+            //if (jQuery(`.kpic-data-item[data-uid="${item.uid}"]`).length > 0) {
+            return { label: item.label, color: item.color, cssClass: item.cssClass, uid: item.uid }
+
+        }
+        selector = selector || this.legendSelector;
+        if (selector && jQuery(selector).length > 0) {
+            let legendArray = this.kpiItems.map(getLegend);
+            let answer = [];
+            // get only unique data elements for elements on the page 
+
+            legendArray.forEach(item => {
+                if (jQuery(`.kpic-data-item[data-uid="${item.uid}"]`).length > 0) {
+                    if (!answer.some(y => y.label === item.label)) {
+                        answer.push({
+                            label: item.label,
+                            color: item.color,
+                            cssClass: item.cssClass
+                        })
+                    }
+                }
+            })
+            jQuery(selector).addClass('kpic-legend').show().empty().append('<div class="kpic-legend-items"></div>');
+            answer.forEach(item => {
+                var style = (item.color) ? `style="color:${item.color};"` : ""
+                var elem = `<div class="kpic-legend-item">
+                <span ${style} class="k-icon k-i-circle ${item.cssClass}"></span>
+                ${item.label}
+            </div>`;
+                jQuery(selector).find('.kpic-legend-items').append(elem);
+            });
+        }
     }
 
     // Private Methods 
@@ -87,19 +149,19 @@ class kpiCalendar {
             this.monthDisplay.callback(this.activeDate);
         }
     }
-    #validMonth (month) {
+    #validMonth(month) {
         var d = new Date();
         if (typeof month === 'undefined' || parseInt(month) < 0 || parseInt(month) > 11) {
             return d.getMonth();
         }
         return month
     }
-    #validYear (year) {
+    #validYear(year) {
         var d = new Date();
         if (typeof year === 'undefined' || parseInt(year) < 2000) {
             return d.getFullYear();
         }
-        return year 
+        return year
     }
 
     #addKpiDomItem(item) {
@@ -116,22 +178,30 @@ class kpiCalendar {
         var selector = `.kpic-box[data-date="${item.key}"]`;
         var itemSelector = `.kpic-data-item[data-uid="${item.uid}"]`;
         var $box = this.elem.find(selector);
+
         if ($box.length > 0 && jQuery(itemSelector).length === 0) {
-            var e = 
-            `<div data-uid="${item.uid}" class="${cssClass}" title="${item.title}" style="${cssStyle}">
-                <div class="kpic-data-item-inner">${item.text}</div>
-            </div>`;
-            this.elem.find(selector).addClass('kpic-has-items').find('.kpic-data-items').append(e);
+
+            if (this.type === 'fill') {
+                // add only the last data item for a calendar of type fill.
+                $box.find('.kpic-data-items .kpic-data-item').remove()
+            }
+
+            var e = `<div data-uid="${item.uid}" class="${cssClass}" title="${item.title}" style="${cssStyle}">
+                        <div class="kpic-data-item-inner">${item.text}</div>
+                    </div>`;
+            $box.addClass('kpic-has-items').find('.kpic-data-items').append(e);
 
             // calculate whether there are kpi items that are below the bottom (hidden) of the date box.             
-            var boxBottom =  $box.offset().top + $box.innerHeight();
-            var lastItem = $box.find('.kpic-data-item:last-child') ;
-            if (lastItem.offset().top > boxBottom) {
+            var boxBottom = $box.offset().top + $box.innerHeight();
+            var lastItem = $box.find('.kpic-data-item:last-child');
+            var bottom = lastItem.offset().top + lastItem.innerHeight();
+            if (bottom > boxBottom) {
+                // store this in DOM, so that we no exactly how many are *not* shown, as opposed to how many there are. 
                 var count = parseInt($box.find('.kpic-data-items-count').text()) || 0;
                 $box.find('.kpic-data-items-count').show().text(`+${count + 1}`);
             }
-        }        
-        
+            this.#buildLegend();
+        }
     }
 
     #buildCalendarElement() {
@@ -139,7 +209,7 @@ class kpiCalendar {
         var today = new Date();
         var thisMonth = this.activeDate.getMonth();
 
-        // build legend calendar header row 
+        // build kpi calendar header row 
         var headerId = this.#uuid();
         this.elem.empty().append(`<div data-uuid="${headerId}" class="kpic-header-row"></div>`);
         var $lgcHeader = jQuery(`[data-uuid="${headerId}"]`);
@@ -188,11 +258,13 @@ class kpiCalendar {
             }
         }
 
-        // if there are elements in the legend, re-add them to the new calendar 
+        // if there are elements in the kpi, re-add them to the new calendar 
         // lovely use of arrow function here, since it doesn't jerk around with 'this'        
-        this.legendItems.forEach( item => {
+        this.kpiItems.forEach(item => {
             this.#addKpiDomItem(item);
         });
+
+        this.#buildLegend();
 
         this.#updateMonthName();
     }
